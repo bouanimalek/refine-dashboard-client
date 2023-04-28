54c7ace6580718d04a8f1e8c73620faab6ec737b
malek <malzovich@gmail.com>
2023-04-28 11:04:19 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <GitHubBanner />
+      {/* <GitHubBanner /> */}
       <RefineKbarProvider>
         <ColorModeContextProvider>
           <CssBaseline />
